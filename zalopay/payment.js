c6277fc05b5685c38d5b0dcadb3158677fbcc002
LinhLT <tanlinh.ace@gmail.com>
2023-11-15 16:16:30 +0700

    Linh: completed zalopay

--- zalopay/payment.js
+++ zalopay/payment.js
@@ -22,7 +22,7 @@ function createZaloPayOrder(describe, amount) {
         amount: amount,
         description: describe,
         bank_code: "zalopayapp",
-        callback_url: "https://7293-2402-800-629c-7db7-dc14-7738-27ea-5b58.ngrok-free.app/api/order/callback",
+        callback_url: "https://e959-2402-800-629c-ea57-9d73-509a-d6b9-b76b.ngrok-free.app/api/order/callback",
     };
 
     const data = config.appid + "|" + order.app_trans_id + "|" + order.app_user + "|" + order.amount + "|" + order.app_time + "|" + order.embed_data + "|" + order.item;
@@ -76,34 +76,54 @@ function checkZaloPayment(app_trans_id) {
 }
 
 function returnZaloMoney(amount, description, zp_trans_id) {
-    const refund_url = "https://sb-openapi.zalopay.vn/v2/refund";
     const timestamp = Date.now();
     const uid = `${timestamp}${Math.floor(111 + Math.random() * 999)}`;
-    let params = {
+    const params = {
         app_id: config.appid,
-        m_refund_id: `${moment().format('YYMMDD')}_${config.appid}_${uid}`,
-        timestamp,
         zp_trans_id: zp_trans_id,
+        m_refund_id: `${moment().format('YYMMDD')}_${config.appid}_${uid}`,
         amount: amount,
+        timestamp: timestamp,
         description: description,
     };
-
     let data = params.app_id + "|" + params.zp_trans_id + "|" + params.amount + "|" + params.description + "|" + params.timestamp;
     params.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
-    console.log(params.m_refund_id);
-    return axios.post(refund_url, null, { params: params });
+    console.log(params);
+    let configs = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: "https://sb-openapi.zalopay.vn/v2/refund",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: qs.stringify(params)
+    };
+
+    return axios(configs)
 }
 
 function checkZaloReturn(m_refund_id) {
-    const endpoint = "https://sb-openapi.zalopay.vn/v2/query_refund";
+    //const endpoint = "https://sb-openapi.zalopay.vn/v2/query_refund";
     const params = {
         app_id: config.appid,
         timestamp: Date.now(), // miliseconds
         m_refund_id: m_refund_id,
     };
-    const data = config.appid + "|" + params.m_refund_id + "|" + params.timestamp; // app_id|m_refund_id|timestamp
+    let data = config.appid + "|" + params.m_refund_id + "|" + params.timestamp; 
     params.mac = CryptoJS.HmacSHA256(data, config.key1).toString()
-    return axios.post(endpoint, null, { params })
+    //return axios.post(endpoint, null, { params })
+    let postConfig = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://sb-openapi.zalopay.vn/v2/query_refund',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            //'Accept': 'application/json'
+        },
+        data: qs.stringify(params)
+    };
+    console.log(params);
+    return axios(postConfig);
 }
 
 
