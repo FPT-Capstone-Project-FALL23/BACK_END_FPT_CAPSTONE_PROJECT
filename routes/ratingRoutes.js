a5840e3d3eaa54b7c7b7090402b60e1f22cbe569
Son <sondeut123@gmail.com>
2023-12-12 14:33:51 +0700

    fix rating

--- routes/ratingRoutes.js
+++ routes/ratingRoutes.js
@@ -1,4 +1,4 @@
-const  {createRating, deleteRating, getRating } = require('../controllers/ratingController');
+const  {createRating, deleteRating, getRating, getClientRating } = require('../controllers/ratingController');
 
 const ratingRouter = require("express").Router();
 
@@ -6,5 +6,5 @@ const ratingRouter = require("express").Router();
 ratingRouter.post("/createRating", createRating)
 ratingRouter.post("/deleteRating", deleteRating)
 ratingRouter.post("/getRating", getRating)
-
+ratingRouter.post("/getClientRating", getClientRating)
 module.exports = ratingRouter
